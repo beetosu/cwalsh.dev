e5971aee237961c980530b74a3f17ede87eea82e
beetosu <cpwalsh@buffalo.edu>
2020-12-17 14:39:31 -0500

    fixed lil syntax issue

--- makes.js
+++ makes.js
@@ -5,8 +5,8 @@ function customizeList(entries, color) {
       thumbnail[0].style.background = 'url("../img/' + div.id + '.png") center no-repeat';
       thumbnail[0].style.backgroundSize = 'cover';
     } else {
-      div.getElementsByClassName("entry-text")[0].style.maxWidth = "calc(33em + 5vmax)"
-      div.getElementsByClassName("entry-text")[0].style.minHeight = "calc(9em + 5vmin)"
+      div.getElementsByClassName("entry-text")[0].style.maxWidth = "calc(33em + 5vmax)";
+      div.getElementsByClassName("entry-text")[0].style.minHeight = "calc(9em + 5vmin)";
     }
     div.style.backgroundColor = color;
   }
